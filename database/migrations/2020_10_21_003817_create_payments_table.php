286a9461438defc87384e57ac0e8182be440de02
Darío Rivera <fermius.us@gmail.com>
2020-10-20 20:17:02 -0500

    refactor: payment fields

--- database/migrations/2020_10_21_003817_create_payments_table.php
+++ database/migrations/2020_10_21_003817_create_payments_table.php
@@ -19,11 +19,7 @@ class CreatePaymentsTable extends Migration
             $table->string('reference', 32);
             $table->unsignedInteger('request_id')->nullable()->index();
             $table->tinyInteger('status')->default(PaymentStatus::PENDING);
-            $table->text('message')->nullable();
             $table->string('process_url')->nullable();
-            $table->string('payment_method', 16)->nullable();
-            $table->string('payment_method_name', 128)->nullable();
-            $table->string('issuer_name', 128)->nullable();
             $table->string('receipt', 20)->nullable();
             $table->string('authorization', 40)->nullable();
             $table->string('currency', 3)->nullable();
