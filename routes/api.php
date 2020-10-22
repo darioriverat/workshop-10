126066280ba866f439f77a9af42ab1873e9efd92
Darío Rivera <fermius.us@gmail.com>
2020-10-21 23:22:55 -0500

    feat: add api mock

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/session', 'App\Http\Controllers\Api\ApiController@session');
+Route::get('/info/{id}', 'App\Http\Controllers\Api\ApiController@info');
+
